9a9598f3c17738d730c3d044bccc974ed44c20f5
Kashif <kashifd.tanikatech@gmail.com>
2025-03-09 22:23:37 +0530

    remove user validation from review route

--- routes/reviews/index.js
+++ routes/reviews/index.js
@@ -3,7 +3,7 @@ const ReviewController = require("../../controllers/review/index.js");
 const { user } = require("../../middleware/authMiddleware.js");
 const router = express.Router();
 
-router.get("/", user, ReviewController.getAllReviews);
+router.get("/", ReviewController.getAllReviews);
 router.post("/", user, ReviewController.postReviews);
 
 module.exports = router;
